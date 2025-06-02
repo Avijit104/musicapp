357a0e9c7f94e2396259d0c6b17ede7d84fa18d2
Avijit Ash <avijitash06@gmail.com>
2025-06-02 13:09:59 +0530

    create playslice reducer

---
+++ src/store/playSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const playInitial = {
+  playstatus: false,
+  playData: null,
+};
+
+const PlaySlice = createSlice({
+  name: play,
+  initialState: playInitial,
+  reducers: {
+    playStart: (state, acttion) => {
+      state.playstatus = true;
+      state.playData = acttion.payload;
+    },
+    playStop: (state, action) => {
+      state.playstatus = false;
+      state.playData = null;
+    },
+  },
+});
+
+export const { playStart, playStop } = PlaySlice.actions;
+export default PlaySlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "./authSlice";
+import PlaySlice from "./playSlice";
 
 const store = configureStore({
   reducer: {
     auth: AuthSlice,
+    play: PlaySlice,
   },
 });
-export default store
+export default store;
